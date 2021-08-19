cd019e0411b5883f40d7418e7568736b1c3f2566
simolex <34382329+simolex@users.noreply.github.com>
2021-08-19 17:56:32 +0300

    Завершено третье задание

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  var currentFloor = 2;
+  var floorPath = $(".home-image path");
+  var counterUp = $(".counter-up");
+  var counterDown = $(".counter-down");
+  floorPath.on("mouseover", function () {
+    floorPath.removeClass("current-floor");
+    currentFloor = $(this).attr("data-floor");
+    $(".counter").text(currentFloor);
+    console.log();
+  });
+  counterUp.on("click", function () {
+    if (currentFloor < 18) {
+      currentFloor++;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGroupping: false,
+      });
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+  counterDown.on("click", function () {
+    if (currentFloor > 2) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGroupping: false,
+      });
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+});
