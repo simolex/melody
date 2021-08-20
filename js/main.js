de636bc66c064563a0b79a33d4b63356410b947c
simolex <34382329+simolex@users.noreply.github.com>
2021-08-20 19:53:01 +0300

    дополнительное задание для 4го урока

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 $(document).ready(function () {
   var currentFloor = 2;
   var floorPath = $(".home-image path");
+
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
   var modal = $(".modal");
   var modalCloseButton = $(".modal-close-button");
   var viewFlatsButton = $(".view-flats");
+  var flatPath = $(".flats path");
+  var flatInfo = $(".flat-link");
 
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
     $(".counter").text(currentFloor);
+    console.log();
+  });
+
+  flatPath.on("mouseover", function () {
+    flatPath.removeClass("current-flat");
+    flatInfo.removeClass("current-flat");
+    currentFlat = $(this).attr("data-flat");
+    $(`[data-flat=${currentFlat}]`).addClass("current-flat");
+    console.log();
+  });
+
+  flatInfo.on("mouseover", function () {
+    flatPath.removeClass("current-flat");
+    flatInfo.removeClass("current-flat");
+    currentFlat = $(this).attr("data-flat");
+    $(`[data-flat=${currentFlat}]`).addClass("current-flat");
     console.log();
   });
 
